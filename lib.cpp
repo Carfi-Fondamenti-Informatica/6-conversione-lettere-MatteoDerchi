cbe7c0dba212c4a6bef4f6e6278f744e4b900f4c
MatteoDerchi <91695814+MatteoDerchi@users.noreply.github.com>
2021-11-30 14:24:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
 
+//
+// Created by Matteo on 29/11/2021.
+//
+
+#include "lib.h"
+bool conversione (char &a) {
+    bool t = true;
+    if (a >= 65 & a<=90 or a>=97 & a<=122) {
+        t=true;
+        if (a >= 65 & a <= 90) {
+            a=a+32;
+            return &a;
+
+        }else {
+            a=a-32;
+            return &a;
+        }
+    }else {
+        t=false;
+    }
+    return t;
+}
